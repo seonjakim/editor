b702480e527af4a50f1a701a26745cfbd67ea078
seonja <seonja.kim0@gmail.com>
2021-07-26 21:48:24 +0900

    test parcel

--- react-editor/webpack.config.js
+++ react-editor/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_module/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
       },
       {
