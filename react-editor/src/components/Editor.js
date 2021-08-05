bf8403e0f078cc53d76fc19aff8cf026d7c385b0
seonja <seonja.kim0@gmail.com>
2021-08-05 15:42:07 +0900

    check bundler files

--- react-editor/src/components/Editor.js
+++ react-editor/src/components/Editor.js
@@ -1,34 +1,37 @@
-import React, { useState } from 'react'
-import { toMarkdown } from '../../../lib/parser'
-import '../../../styles/style.css'
+import React, { useState } from 'react';
+import { toMarkdown } from '../../../lib/parser';
+import '../../../styles/style.css';
 
 const Editor = () => {
   /** create buttons */
-  const buttonList = ['heading', 'bold', 'italic', 'strike', 'hrline', 'quote']
-  const toolbar = buttonList.map((el, idx) => (
-    <button key={idx} className={`toolbar-icons ${el}-i`}></button>
-  ))
+  const buttonList = ['heading', 'bold', 'italic', 'strike', 'hrline', 'quote'];
+  const toolbar = buttonList.map((el, idx) => <button key={idx} className={`toolbar-icons ${el}-i`}></button>);
   /** get input from textarea and parsing it to new tag form */
-  let defaultContent =
-    '# This is h1\n## This is h2\n### This is h3\n>This is Quote\nThis is plain text'
-  const [inputValue, setInputValue] = useState(defaultContent)
-  let markdownObject = toMarkdown(inputValue)
+  let defaultContent = '# This is h1\n## This is h2\n### This is h3\n>This is Quote\nThis is plain text';
+  const [inputValue, setInputValue] = useState(defaultContent);
+  let markdownObject = toMarkdown(inputValue);
   let contents = markdownObject.map((el, idx) => (
     <div key={idx} className={el.tag}>
       {el.text}
     </div>
-  ))
+  ));
+
+  const setTest = (e) => {
+    console.log(e.target.value);
+  };
 
   return (
-    <div className='container'>
-      <nav className='toolbar-box'>{toolbar}</nav>
+    <div className="container">
+      <nav className="toolbar-box">{toolbar}</nav>
       <textarea
         onChange={(e) => setInputValue(e.target.value)}
-        className='get-input'
+        className="get-input"
         defaultValue={defaultContent}
       ></textarea>
-      <div className='output'>{contents}</div>
+      <div contentEditable="true" onChange={(e) => setTest(e)} className="output">
+        {contents}
+      </div>
     </div>
-  )
-}
-export default Editor
+  );
+};
+export default Editor;
