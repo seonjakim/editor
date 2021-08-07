e0285f51debfa9c846908388da688d8d05e3efa2
seonja <seonja.kim0@gmail.com>
2021-08-07 11:45:29 +0900

    delete previous files and structure folders

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import Editor from '../components/Editor'
-import Frame from '../components/Frame'
 
 const App = () => {
-  return (
-    <Frame>
-      <Editor />
-    </Frame>
-  )
+  return <div></div>
 }
 export default App
