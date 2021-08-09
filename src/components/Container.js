4db0dfde422723c8451222676f0f876bf60f77ad
seonja <seonja.kim0@gmail.com>
2021-08-09 11:36:13 +0900

    spinbox background container done

---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className='container-background'>
+      <div className='spinbox-container'>
+        <div className='spinbox-window'></div>
+        <div className='add-spinbox-area'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Container
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import Container from '../components/Container'
 
 const App = () => {
-  return <div></div>
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  )
 }
 export default App
